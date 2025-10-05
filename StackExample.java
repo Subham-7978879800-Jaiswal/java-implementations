5a9774f323e0318a62e026cf478edf54a5a0c730
Subham-7978879800-Jaiswal <subhamjaiswalamell@gmail.com>
2025-10-05 21:28:07 +0530

    Stack Starter Implementation

---
+++ StackExample.java
@@ -0,0 +1,10 @@
+import java.util.Stack;
+
+class StackExample{
+    public static void main(String args[]){
+        Stack<Integer> st = new Stack<Integer>(); 
+        st.add(1);
+        st.add(2); 
+        System.out.println(st);
+    }
+}
